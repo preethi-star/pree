26c2e0fe86d5ff86022abc35180fd11ebb263841
Abhishek <Abhishek@LAPTOP-1J6L9E6I>
2024-08-15 14:37:28 +0530

    Branch commit

---
+++ src/script/demo1.java
@@ -0,0 +1,15 @@
+package script;
+
+public class demo1 {
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+}
